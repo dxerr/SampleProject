4bc70380e98ce7d7e6b2bb973daf47ca303060c1
july1981 <july1981@naver.com>
2026-07-03 10:24:53 +0900

    [Engine] 빌드 타겟 버전을 고정하지 않고 Latest 로 변경

--- Source/ExFrameWorkEditor.Target.cs
+++ Source/ExFrameWorkEditor.Target.cs
@@ -6,7 +6,7 @@ public class ExFrameWorkEditorTarget : TargetRules
 	public ExFrameWorkEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V7;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("ExFrameWork");
 	}
